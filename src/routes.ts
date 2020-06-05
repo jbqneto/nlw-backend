2d7a83eb206297e0d5d6ccd10017a6cc8a296397
jbqneto <jbqneto@gmail.com>
2020-06-05 11:15:09 -0300

    server and routes updates do add point

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,61 @@
 import express from 'express';
+import knex from './database/connection';
 
-const router = express.Router();
+const routes = express.Router();
 
-export default router;
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  });
+
+  return response.json(serializedItems);
+});
+
+
+routes.post('/points', async (request, response) => {
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+
+  try {
+    const transaction = await knex.transaction();
+
+    const insertedPointIds = await transaction('points').insert({
+      name,
+      email,
+      image: '',
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf
+    });
+
+    const point_id = insertedPointIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+      return {
+        item_id,
+        point_id
+      }
+    });
+
+    console.log(pointItems);
+
+    const pointItemsInserted = await transaction('point_items').insert(pointItems);
+
+    await transaction.commit();
+
+    return response.status(201).json({ point_id, pointItemsInserted });
+
+  } catch (error) {
+    console.log(error);
+    return response.status(error.status || 500).json(error.message || 'Erro ao gravar ponto');
+  }
+
+});
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import router from './routes';
 
@@ -6,5 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(3333);
+app.listen(3000);
